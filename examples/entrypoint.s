27c06571a048b08c250d2821f65dfbb0858b237c
Johan T. Halseth <johanth@gmail.com>
2023-12-20 11:28:25 +0100

    asm: remove a0 writes

--- examples/entrypoint.s
+++ examples/entrypoint.s
@@ -11,15 +11,12 @@ _start:
 	j _sys_halt
 
 __start:
-
 	addi	sp,sp,-32	# allocate 32 bytes on the stack
 	sw	ra,28(sp)	# store ra on the stack
 	sw	s0,24(sp)	# store s0 on the stack
 	addi	s0,sp,32	# store old stack pointer into s0
-	li	a0,2		# load 2 into a0
 	call	runcontract	# call runcontract with a0=x=2
 	sw	a0,-24(s0)	# store a0=y (return value from runcontract) on the stack
-	li	a0,0		# load 0 into a0
 	lw	ra,28(sp)	# load original ra from stack
 	lw	s0,24(sp)	# load original s0 from stack
 	addi	sp,sp,32	# deallocate 32 bytes from stack
@@ -27,6 +24,5 @@ __start:
 
 _sys_halt:
 	add	a1,sp,24
-	sll     a0,a0,0x8
 	li      t0,0
 	ecall
